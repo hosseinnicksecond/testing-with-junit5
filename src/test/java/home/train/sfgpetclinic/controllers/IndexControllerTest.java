c04028651597fdd1a7a271130afcb9ea3167d2e9
hossein nick <hossinnick62@gmail.com>
2020-10-26 20:58:47 +0330

    create IndexControllerTest and use simple assert

---
+++ src/test/java/home/train/sfgpetclinic/controllers/IndexControllerTest.java
@@ -0,0 +1,27 @@
+package home.train.sfgpetclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexControllerTest {
+
+    IndexController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller= new IndexController();
+    }
+
+    @Test
+    void index() {
+
+        assertEquals("index",controller.index(),"if fail this message appear");
+    }
+
+    @Test
+    void oupsHandler() {
+        assertTrue("notimplementedy".equals(controller.oupsHandler()),()->"make some message ");
+    }
+}
