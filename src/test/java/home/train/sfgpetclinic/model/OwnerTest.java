918ed06e4f05dea572d4c8f765f0d7bfc4492e5c
hossein nick <hossinnick62@gmail.com>
2020-10-26 21:50:24 +0330

    create OwnerTest and use Dependency Assertion

---
+++ src/test/java/home/train/sfgpetclinic/model/OwnerTest.java
@@ -0,0 +1,24 @@
+package home.train.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerTest {
+
+    @Test
+    void dependencyAssertions() {
+        Owner owner = new Owner(1l, "John", "Doe");
+        owner.setCity("Key West");
+        owner.setTelephone("12345 678");
+
+        assertAll("Properties Test",
+                () -> assertAll("Person property test",
+                        () -> assertEquals("John", owner.getFirstName(), "firstName dose not match"),
+                        () -> assertEquals("Doe", owner.getLastName(), "lastName Dose not Match")),
+                () -> assertAll("owner Properties Test",
+                        () -> assertEquals("Key West", owner.getCity(), "city name dose not match"),
+                        () -> assertEquals("12345 678", owner.getTelephone(), "telephone not match"))
+        );
+    }
+}
