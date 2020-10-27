72ec1c6240e7a1afa90392cdcbae5a2aaf266b5d
hossein nick <hossinnick62@gmail.com>
2020-10-27 18:47:04 +0330

    add repeated test to OwnerTest

--- src/test/java/home/train/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/home/train/sfgpetclinic/model/OwnerTest.java
@@ -1,6 +1,8 @@
 package home.train.sfgpetclinic.model;
 
 import home.train.sfgpetclinic.ModelTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -23,5 +25,12 @@ class OwnerTest implements ModelTest {
                         () -> assertEquals("Key West", owner.getCity(), "city name dose not match"),
                         () -> assertEquals("12345 678", owner.getTelephone(), "telephone not match"))
         );
+    }
+
+    @RepeatedTest(value = 10,name = "{displayName} : {currentRepetition} - {totalRepetitions}")
+    @DisplayName("repeated test")
+    @Test
+    void repeatedTest() {
+        System.out.println("hello");
     }
 }
